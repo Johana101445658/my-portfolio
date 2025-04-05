e643e07e4c4f4934e1c2c3a40f89a32c7cda0c2d
Johana101445658 <Lizette.Romero@georgebrown.ca>
2025-04-05 19:31:20 -0400

    Update all folders and files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-import path from 'path';
-
-export default {
-  webpack(config) {
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
-    return config;
-  },
-};
+module.exports = {
+  extends: 'next/core-web-vitals', 
+}
