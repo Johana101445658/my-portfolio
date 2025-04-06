ae7857028cc2b88fddf52b95c2ab524e9c99676e
Johana101445658 <Lizette.Romero@georgebrown.ca>
2025-04-05 23:55:21 -0400

    Revert "Update all folders and files"
    This reverts commit e643e07e4c4f4934e1c2c3a40f89a32c7cda0c2d.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  extends: 'next/core-web-vitals', 
-}
+import path from 'path';
+
+export default {
+  webpack(config) {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
+};
