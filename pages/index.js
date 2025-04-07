c97c75b152a6c1f9a2e529da81df39b4d5a71370
Johana101445658 <156722379+Johana101445658@users.noreply.github.com>
2025-04-07 16:44:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '../styles/mystyles.module.css';
-
-const teststyle = {
-  color: "white",
-      backgroundColor: "DodgerBlue",
-      padding: "10px",
-      fontFamily: "Arial"
-};
 
 const Home = () => {
   return (
     <div style={{ backgroundColor: '#ADD8E6', color: '#000', minHeight: '100vh', padding: '20px' }}>
-      <h1 className={styles.johana} >Welcome to My Portfolio</h1>
-      <p style={teststyle}>Hi, I'm Johana Romero, a Computer Programming and Analysis student.</p>
-      <nav id="barra">
+      <h1>Welcome to My Portfolio</h1>
+      <p>Hi, I'm Johana Romero, a Computer Programming and Analysis student.</p>
+      <nav>
         <ul>
           <li><Link href="/projects"><a>Projects</a></Link></li>
           <li><Link href="/experience"><a>Experience</a></Link></li>
