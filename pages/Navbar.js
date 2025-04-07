557d855bc07391785270ecce2a5a8d9a13669f22
Johana101445658 <156722379+Johana101445658@users.noreply.github.com>
2025-04-07 17:35:37 -0400

    Navbar.js

---
+++ pages/Navbar.js
@@ -0,0 +1,18 @@
+// pages/Navbar.js
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About Me</Link></li>
+        <li><Link href="/projects">Projects</Link></li>
+        <li><Link href="/experience">Experience</Link></li>
+        <li><Link href="/resume">Resume</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
